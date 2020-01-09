2a928ae6b4ca1143b80401fcd3421a3baba4325c
Colonizator <art8573669@gmail.com>
2020-01-09 21:37:28 +0300

    add new func to progression game

--- src/games/progrGame.php
+++ src/games/progrGame.php
@@ -6,23 +6,27 @@ use function BrainGames\engine\startEngine;
 
 define('PROGR_RULE', 'What number is missing in the progression?');
 define('LENGTH', 10);
-
+function generateProgression($length)
+{
+    $startProgression = mt_rand(1, 20);
+    $diffProgression = mt_rand(1, 10);
+    $results = [];
+    for ($i = 0; $i < $length; $i++) {
+        $results[$i] = $startProgression + $diffProgression * $i;
+    }
+    return $results;
+}
 function getProgrGameQuestionsAndAnswers()
 {
     $result = [];
     for ($i = 1; $i <= GAMESCOUNT; $i++) {
-        $startProgression = mt_rand(1, 20);
-        $diffProgression = mt_rand(1, 10);
+        $progression = generateProgression(LENGTH);
         $unknownNumPosition = mt_rand(1, LENGTH);
-        $progression = [];
-        for ($j = 0; $j < LENGTH; $j++) {
-            $progression[$j] = $startProgression + $diffProgression * $j;
-        }
-        $unknown = $progression[$unknownNumPosition];
+        $answer = $progression[$unknownNumPosition];
         $progression[$unknownNumPosition] = "..";
         $strProgression = implode(" ", $progression);
         $question = "$strProgression";
-        $result[$question] = $unknown;
+        $result[$question] = $answer;
     }
     return $result;
 }
